66970e62fb157d5e4ebdab296bf35127e7f7245b
DKMld <miti_01@abv.bg>
2022-12-25 00:12:14 +0200

    'heroku_deploy'

--- Selldim/asgi.py
+++ Selldim/asgi.py
@@ -1,14 +1,14 @@
 import os
-
 import django
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from Selldim.chat import routing as chat_routing
+from Selldim import settings
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Selldim.settings')
-django.setup()
+
 
 application = ProtocolTypeRouter(
     {
